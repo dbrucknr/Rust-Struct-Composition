1f853dc9ee63736be10a01b9c9ad324d6f68b1f1
Derek Bruckner <dbrucknr@umich.edu>
2025-09-26 15:45:12 -0400

    Compose structs and traits from within a module struct.

--- src/instances/mod.rs
+++ src/instances/mod.rs
@@ -1,3 +1,36 @@
 pub mod controller;
 pub mod repository;
 pub mod service;
+
+use std::net::IpAddr;
+
+use crate::instances::controller::instance::IpAddrController;
+use crate::instances::repository::instance::IpAddrRepository;
+use crate::instances::service::instance::IpAddrService;
+
+pub struct IpModule {
+    controller: IpAddrController<IpAddrService<IpAddrRepository>>,
+}
+impl IpModule {
+    pub fn new(ip_addrs: Vec<IpAddr>) -> Self {
+        let repository = IpAddrRepository::new(ip_addrs);
+        let service = IpAddrService::new(repository);
+        let controller = IpAddrController::new(service);
+
+        Self { controller }
+    }
+
+    // Borrow to keep module alive:
+    pub fn controller(&self) -> &IpAddrController<IpAddrService<IpAddrRepository>> {
+        &self.controller
+    }
+
+    pub fn controller_mut(&mut self) -> &mut IpAddrController<IpAddrService<IpAddrRepository>> {
+        &mut self.controller
+    }
+
+    // Move if running an app context:
+    pub fn into_controller(self) -> IpAddrController<IpAddrService<IpAddrRepository>> {
+        self.controller
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,18 @@ mod traits;
 
 use std::net::{IpAddr, Ipv4Addr};
 
-use instances::controller::instance::IpAddrController;
-use instances::repository::instance::IpAddrRepository;
-use instances::service::instance::IpAddrService;
-
-use crate::traits::controller::Controller;
+use crate::instances::IpModule;
+use crate::traits::controller::Controller; // Import this trait so we can use the methods
 
 fn main() {
-    let respository = IpAddrRepository::new(Vec::from([
+    let mut ip_module = IpModule::new(Vec::from([
         IpAddr::V4(Ipv4Addr::new(192, 0, 0, 1)),
         IpAddr::V4(Ipv4Addr::new(192, 0, 0, 2)),
         IpAddr::V4(Ipv4Addr::new(192, 0, 0, 3)),
     ]));
 
-    let service = IpAddrService::new(respository);
-    let mut controller = IpAddrController::new(service);
+    let controller = ip_module.controller_mut();
 
-    // let all = controller.index();
     if let Ok(result) = controller.index() {
         println!("All Values: {:?}", result);
     }
@@ -29,7 +24,7 @@ fn main() {
     }
 
     if let Ok(addr) = controller.patch(
-        &mut IpAddr::V4(Ipv4Addr::new(192, 0, 0, 2)),
+        &IpAddr::V4(Ipv4Addr::new(192, 0, 0, 2)),
         IpAddr::V4(Ipv4Addr::new(192, 0, 0, 102)),
     ) {
         println!("Updated: {:?}", addr);
